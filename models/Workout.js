8fb38eaf12c9f03f05e5901147ef1b212d33bb1c
Adrian Aguilar <Tygor23@gmail.com>
2021-08-05 13:24:39 -0700

    Adding server.js and Workout model

---
+++ models/Workout.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date ()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: 'Enter an exercise type'
+        },
+        name: {
+            type: String,
+            required: 'Enter exercise name'
+        },
+        duration: {
+            type: Number,
+            required: 'Enter duration'
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+});
+const Workout = mongoose.model('Workout', workoutSchema)
+module.exports = Workout
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require ("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
